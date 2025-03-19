6577a7ad6fb8d39896c8d98734cd6b2ee0bf9f08
Isa <kimtoba890@gmail.com>
2025-03-19 16:51:17 -0300

    Update carro.py

--- carro.py
+++ carro.py
@@ -1,37 +1,68 @@
 class Carro:
-    def __init__(self, modelo, ano, cor):
+    def __init__(self, marca, modelo, ano, cor, limite_velocidade):
+        self.marca = marca
         self.modelo = modelo
         self.ano = ano
         self.cor = cor
-        self.combustivel = 20
+        self.combustivel = 10
         self.ligado = False
         self.velocidade = 0
+        self.limite_velocidade = limite_velocidade
 
     def ligar (self):
-        if self.ligado is False:
+        if self.combustivel > 0:
             self.ligado = True
-            print(f"o carro {self.modelo} ligou.")
+            print(f"O {self.marca} {self.modelo} ligou.")
         else:
-            print(f"O carro {self.modelo} já está ligado.")
+            print(f"O {self.marca} {self.modelo} está sem gasolina")
 
     def desligar (self):
         if self.ligado is True:
             self.ligado = False
-            print(f"o carro {self.modelo} desligou.")
+            print(f"{self.marca} {self.modelo} desligou.")
         else:
-            print(f"O carro {self.modelo} já está desligado.")
+            print(f"{self.marca} {self.modelo} já está desligado.")
 
     def acelerar (self):
         if self.ligado is True:
             if self.combustivel >= 5:
-                self.combustivel -= 5
-                self.velocidade += 10
-                print(f'o {self.modelo} acelerou!')
+                if self.velocidade <= self.limite_velocidade:
+                    self.velocidade += 10
+                    self.combustivel -= 5
+                    print(f"{self.modelo} acelerou!")
+                else:
+                    print(f"{self.modelo} está no limite de velocidade")
+            else:
+                print(f"{self.modelo} está sem combustível.")
         else:
-            print(f"O carro {self.modelo} está desligado e não pode acelerar.")
+            print(f"{self.modelo} está desligado e não pode acelerar.")
+
+    def frear (self):
+        if self.velocidade > 0:
+            self.velocidade -= 10
+            print(f"{self.modelo} freou, {self.velocidade} km/h.")
+        else:
+            print(f"{self.modelo} já está parado")
 
     def abastecer (self, quantidade):
-        self.combustivel += quantidade
-        
+        if self.combustivel + quantidade < 100:
+            self.combustivel += quantidade
+            print(f"{self.modelo} foi abstecido, combustível: {self.combustivel}")
+        else:
+            print(f"Abastecimento acima do limite do combustível.")
 
-        
+    def buzinar (self):
+        print("BI BI BIIIIIIIIII")
+
+carro1 = Carro ("Mercedes-Benz", "C180", 2020, "Preto", 223)
+
+carro1.acelerar()
+carro1.ligar()
+carro1.abastecer(80)
+for i in range (3):
+    carro1.acelerar()
+for i in range (1):
+    carro1.frear()
+carro1.acelerar()
+carro1.buzinar()
+carro1.desligar()
